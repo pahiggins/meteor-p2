a1d7b8aca79cbf127436fc4b3fbf697cd54763f2
Peter Higgins <pah@cenabo.com>
2018-04-18 15:57:27 +0100

    Add EmployeeList and EmployeeDetail components. Wire up associated pub / sub system.

--- client/main.js
+++ client/main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import EmployeeList from './components/EmployeeList';
+
 const App = () => {
   return (
     <div>
-      App
+      <EmployeeList />
     </div>
   );
 };
